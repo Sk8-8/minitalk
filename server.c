f0ca3d907a53155d3f567317b4cabd9e4e5a1966
Kerloye Guillemin <kguillem@c1r1p10.42perpignan.fr>
2025-03-14 23:00:02 +0100

    ongoing

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: kguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 17:00:41 by kguillem          #+#    #+#             */
-/*   Updated: 2025/03/14 19:59:40 by kguillem         ###   ########.fr       */
+/*   Updated: 2025/03/14 22:55:14 by kguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,29 @@
 #include <signal.h>
 #include <unistd.h>
 
-static char	*msg; //
+static char	*g_msg;
+// SIGUSR1
+// SIGUSR2
+// SIGINT
+// SIGTERM
 
-void	signal_handler(int	signum)
+void	signal_handler(int signum)
 {
-	ft_printf("%s", "signal received!\n");
-	ft_printf("%d", signum);
+	sigaction();
+	ft_printf("signal received!\n%d\n", signum);
 }
 
-int	initialize(void)
+void	initialize(void)
 {
 	int	pid;
 
-	pid = getpid(void);
-	ft_printf("%d",pid);
-	//sigaction(SIGUSR1, 
-	return (0);
+	pid = getpid();
+	ft_printf("%d", pid); 
+	pause();
 }
-
 
 int	main(void)
 {
-	initialize;
+	initialize();
+	
 }
