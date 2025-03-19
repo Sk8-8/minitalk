c79028bd450e1a8bff26306766618554a7e6cbd3
Kerloye Guillemin <kguillem@c1r1p10.42perpignan.fr>
2025-03-19 18:17:43 +0100

    near ending

--- server.c
+++ server.c
@@ -6,73 +6,71 @@
 /*   By: kguillem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 17:00:41 by kguillem          #+#    #+#             */
-/*   Updated: 2025/03/17 21:21:25 by kguillem         ###   ########.fr       */
+/*   Updated: 2025/03/19 17:37:21 by kguillem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <sys/types.h>
 #include <signal.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-static char	*g_msg;
-/*
-struct	sigaction {
-void	(*sa_handler) (int);
-void	(*sa_sigaction) (int, siginfo_t *, void *);
-sigset_t	SIGINFO;
-int	sa_flags;
-void	(*sa_restorer) (void);
-};
-*/
-void	msghandler(char *g_msg)
+static char	g_octet;
+
+void	signal_handler(int signum)
 {
-	unsigned int	i;
-	unsigned int	bit;
+	if (signum == SIGUSR1)
+		g_octet = g_octet + 1;
+	if (signum == SIGUSR2)
+		g_octet = g_octet + 0;
+}
 
-struct	sigaction {
-void	(*signal_handler) (int);
-void	(*sa_sigaction) (int, siginfo_t *, void *);
-int	SIGINFO;
-sigset_t	SIGINFO;
-void	(*sa_restorer) (void);
+char	*oneupper(char	*msg)
+{
+	char	*oneupper;
+	int		i;
+
+	i = 0;
+	while (msg)
+		i ++;
+	oneupper = malloc(sizeof(char) * (i + 1));
+	if (oneupper == NULL)
+		return (NULL);
+	while (i != 0)
+	{
+		oneupper[i] = msg[i];
+		i --;
+	}
+	free(msg);
+	return (oneupper);
+}
+
+char	*msghandler(void)
+{
+	char	*msg;
+	int		i;
+	int		bit;
 
 	i = 0;
 	bit = 0;
-	while (g_msg)
+	g_octet = '?';
+	while (g_octet != '\0')
 	{
+		g_octet = '\0';
+		msg = oneupper(msg);
 		while (bit < 7)
 		{
-			g_msg[i] = g_msg[i] +
-			g_msg[i] << 1;
+			signal_handler();
+			g_octet = g_octet << 1;
 			bit ++;
 		}
-		g_msg[i] = g_msg[i] +
+		signal_handler();
+		msg[i] = g_octet;
 		bit = 0;
 		i ++;
 	}
-}
-
-void	signal_handler(int signum, siginfo_t *info, ucontext_t *uap)
-{
-	int	bit;
-	char ascii;
-
-	bit = 0;
-	ascii = 0;
-	while (bit < 8)
-	{
-		if (signum == SIGUSR1)
-		{
-			ascii = ascii + 1;
-			ascii << 1;
-			bit ++;
-		}
-		if (signum == SIGUSR2)
-		{
-			ascii << 1;
-			bit ++;
-		}
-	}
+	return (msg);
 }
 
 void	initialize(void)
@@ -80,12 +78,18 @@ void	initialize(void)
 	int	pid;
 
 	pid = getpid();
-	ft_printf("%d", pid); 
+	printf("%d", pid);
 	pause();
 }
 
 int	main(void)
 {
+	char	*msg;
+
 	initialize();
-	
+	while (1)
+	{
+		msg = msghandler();
+		printf("%s\n", msg);
+	}
 }
